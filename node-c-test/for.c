1bb7021933f1b8489aacf5d5889762c7f18ab9e7
Koen Vendrik <k.vendrik@gmail.com>
2015-11-06 18:39:37 +0200

    Adds iterations

--- node-c-test/for.c
+++ node-c-test/for.c
@@ -3,7 +3,13 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-	for(int i = 0; i < 1000; i++){
-		printf("%i", rand());
+	int r = 0;
+	for(int i = 0; i < 24000000; i++){
+		int add = 0;
+		for(int j = 0; j < 25; j++){
+			add *= rand();
+		}
+		r += add;
 	}
+	printf("%i", r/2);
 }
--- node-c-test/pure-node.js
+++ node-c-test/pure-node.js
@@ -1,3 +1,9 @@
-for(var i = 0; i < 24000; i++){
-	console.log(Math.random());
+var number = 0;
+for(var i = 0; i < 24000000; i++){
+	var add = 0;
+	for(var j = 0; j < 25; j++){
+		add *= Math.random();
+	}
+	number += add;
 }
+console.log(number/2);
