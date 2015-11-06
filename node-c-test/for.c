76e37cd172cc3460e5ca57f3d603a1b2d1e14f2d
Koen Vendrik <k.vendrik@gmail.com>
2015-11-06 18:26:37 +0200

    initial

---
+++ node-c-test/for.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+	for(int i = 0; i < 1000; i++){
+		printf("%i", rand());
+	}
+}
---
+++ node-c-test/pure-node.js
@@ -0,0 +1,3 @@
+for(var i = 0; i < 24000; i++){
+	console.log(Math.random());
+}
